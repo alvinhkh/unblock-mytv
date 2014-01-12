52667f90d7c520a700e00c45a43f9b2100088c8e
AlvinHKH <email@alvinhkh.com>
2014-01-12 15:53:03 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Unblock myTV
+
+A chrome extension that bypass blocks of oversea connection of myTV player in tvb.com
+
+
+## Technical details
+
+- Uses [X-Forwarded-For](http://en.wikipedia.org/wiki/X-Forwarded-For) to pretend request connection from accessible places (Hong Kong).
+- Uses [declarativeWebRequest](https://developer.chrome.com/extensions/declarativeWebRequest.html) API (in beta and dev channel, as of Jan 2014).
+ - Unlike [chrome.webRequest](https://developer.chrome.com/extensions/webRequest.html), [declarativeWebRequest](https://developer.chrome.com/extensions/declarativeWebRequest.html) + [event page](http://developer.chrome.com/extensions/event_pages.html) makes the extension loaded only when they are needed, which means no background pages and would not hold memory resourses.
+
+## License
+
+	Copyright (c) 2014, AlvinHKH
+	http://alvinhkh.com
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without modification, 
+	are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, 
+	this list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright notice, 
+	this list of conditions and the following disclaimer in the documentation and/or 
+	other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+	IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+	FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+	IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+	OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ event_page.js
@@ -0,0 +1,128 @@
+/**
+ * Range of IPv4 Addresses that Accessible
+ * [FROM, TO]
+ */
+var ip_address_range = [
+	["1.36.0.0", "1.36.255.255"],
+	["1.64.0.0", "1.65.255.255"],
+	["14.198.0.0", "14.198.127.255"],
+	["59.148.0.0", "59.149.255.255"],
+	["121.202.0.0", "121.202.0.255"],
+	["121.203.0.0", "121.203.255.255"],
+	["180.219.0.0", "180.219.255.255"],
+	["183.178.0.0", "183.179.255.255"],
+	["218.102.0.0", "218.103.255.255"],
+	["218.188.0.0", "218.189.255.255"],
+	["221.124.0.0", "221.127.255.255"],	
+];
+
+/**
+ * Returns a random integer between min and max
+ */
+function random_number(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Convert IPv4 Address to number
+ */
+function dot_to_number(dot) {
+	var d = dot.split('.');
+	return ( ( ( ( ( ( +d[0] ) * 256 ) + ( + d[1] ) ) * 256 ) + ( + d[2] ) ) * 256 ) + ( + d[3] );
+}
+
+/**
+ * Convert number to IPv4 Address
+ */
+function number_to_dot(number) {
+	var dot = number % 256;
+	for (var i = 3; i > 0; i--) { 
+		number = Math.floor( number / 256 );
+		dot = number % 256 + '.' + dot;
+	}
+	return dot;
+}
+
+/**
+ * Random select a range from ip_address_range
+ */
+var random_selected_range = ip_address_range[random_number(0, ip_address_range.length-1)];
+
+/**
+ * Random select an ip address in selected range
+ */
+var ip_address = number_to_dot(
+	random_number(
+		dot_to_number(
+			random_selected_range[0]
+		), 
+		dot_to_number(
+			random_selected_range[1]
+		)
+	)
+);
+
+/**
+ * Add Rules to SetRequestHeader for X-Forwarded-For by using
+ * chrome.declarativeWebRequest API
+ */
+var RequestMatcher = chrome.declarativeWebRequest.RequestMatcher;
+var RedirectByRegEx = chrome.declarativeWebRequest.RedirectByRegEx;
+
+function registerRules() {
+	var redirectRule = {
+		priority: 100,
+		conditions: [
+			new RequestMatcher({
+				url: {
+					hostSuffix: '.tvb.com'
+				},
+			}),
+		],
+		actions: [
+			new chrome.declarativeWebRequest.SetRequestHeader({
+				name: 'X-Forwarded-For', 
+				value: ip_address
+			}),
+		]
+	};
+	
+	/**
+	 * Callback function after addRules
+	 */
+	var callback = function() {
+		if (chrome.extension.lastError) {
+			console.error('Error when adding rules:', chrome.extension.lastError.message);
+		} else {
+			console.info('Rules successfully added.');
+			chrome.declarativeWebRequest.onRequest.getRules(null, function(rules) {
+				console.info('Rules that added:', JSON.stringify(rules, null, 2));
+			});
+		}
+	};
+
+	/**
+	 * Add Rules
+	 */
+	chrome.declarativeWebRequest.onRequest.addRules([
+		redirectRule
+	], callback);
+}
+
+function setup() {
+	/**
+	 * Remove previously added rules before adding new ones
+	 */
+	chrome.declarativeWebRequest.onRequest.removeRules(null, function() {
+		if (chrome.extension.lastError) {
+			console.error('Error when clearing rules:', chrome.extension.lastError.message);
+		} else {
+			registerRules();
+		}
+	});
+}
+
+/**
+ * Run when the extension installed / updated
+ */
+chrome.runtime.onInstalled.addListener(setup);
